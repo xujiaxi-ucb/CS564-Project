f6eb94a06123e99b6084832b953bfd4ea8300c15
xujiaxi-ucb <67991755+xujiaxi-ucb@users.noreply.github.com>
2022-11-02 15:32:50 -0500

    finished unPinPage

--- part3/buf.C
+++ part3/buf.C
@@ -116,7 +116,22 @@ const Status BufMgr::readPage(File* file, const int PageNo, Page*& page)
 
 const Status BufMgr::unPinPage(File* file, const int PageNo, 
 			       const bool dirty) 
-
+{
+    int frameNo = 0;
+    // init stage
+    Status status = OK;
+    status = hashTable->lookup(file, PageNo, frameNo);
+    if (status != OK) return HASHNOTFOUND;
+    // the frame containing (file, PageNo)    
+    if (bufTable[frameNo].pinCnt == 0) {
+        return PAGENOTPINNED;
+    }
+    if (dirty) {
+        bufTable[frameNo].dirty == true;
+    }
+    bufTable[frameNo].pinCnt-- ;
+    return OK;
+}
 
 
 
