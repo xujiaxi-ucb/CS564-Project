1f69996a77b52c95c83f8683a5c634459f70b3c2
XuehengWang <71973702+XuehengWang@users.noreply.github.com>
2022-12-12 15:45:29 -0600

    Update insert.C

--- part6/insert.C
+++ part6/insert.C
@@ -15,7 +15,61 @@ const Status QU_Insert(const string & relation,
 	const attrInfo attrList[])
 {
 // part 6
-return OK;
-
+	RID rid;
+	Record rec;
+	InsertFileScan* ifs;
+	Status status;
+	int cnt;
+	AttrDesc *attrs; // an array of AttrDesc
+	
+	status = attrCat -> getRelInfo(relation, cnt, attrs);
+	if (status != OK) return status;
+	
+	// if the counts of attributes are not the same, return OK
+	if (attrCnt!=cnt) {return OK;}
+	
+	// construct the record
+	void* data;
+	AttrDesc current;
+	AttrInfo current_info;
+	int length_data = 0;
+	int find = 0;
+	
+	for (int ind = 0; ind < cnt; ind ++) {
+		current = attrs[ind];
+		
+		// find corresponding attrInfo
+		for (int i = 0; i < attrCnt; i++) {
+			if (attrList[i].attrName == current.attrName && attrList[i].attrType == current.attrType){ 
+				current_info = attrList[i];
+				find = 1;
+			}
+		}
+		
+		// if not find corresponding attrInfo from the input
+		if (find == 0) {return OK;}
+		
+		length_data = length_data + current_info.attrLen;
+		if (ind == 0) { //allocate memory to store the record
+			data = (void *)malloc(length_data);
+		}else { //reallocate memory to store the record
+			data = (void *)realloc(data, length_data);
+		}
+		
+		// copy data
+		memcpy(&data[current.attrOffset], current_info.attrValue, current_info.attrLen);
+		
+		find = 0;
+	}
+	
+	// insertFileScan
+	ifs = new InsertFileScan(relation, status);
+	if (status != OK) return status;
+	
+	// Record
+	rec.length = length_data;
+	rec.data = data;
+	status = ifs->insertRecord(rec, rid);
+	delete ifs;
+  	return status;
 }
-
